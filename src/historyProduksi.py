ad69061eab5c4b4e8ecf50bb751eef6999fcf3f3
Rheyhan <rheyhanfahry@apps.ipb.ac.id>
2024-09-06 22:30:20 +0700

    xD

--- src/historyProduksi.py
+++ src/historyProduksi.py
@@ -1,3 +1,4 @@
+# Ada di historyProduksi.py
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
@@ -12,18 +13,19 @@ years = np.array([1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002,
                   2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 
                   2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020])
 
-produksi = np.array([1646900.00, 1615751.00, 1943709.00, 1971740.00, 1753656.00, 1975700.00, 
-                     1801422.00, 1946406.00, 1992726.00, 1951109.00, 1966293.00, 2091996.00, 
-                     2124144.00, 2129914.00, 2308404.00, 2341075.00, 2673844.00, 2807676.00, 
-                     2940795.00, 3101455.00, 3207002.00, 3320064.00, 3641895.00, 3831923.00, 
-                     4090654.00, 2488641.91, 2164089.33, 2604913.29])
+produksi = np.array([1409559.  , 1347611.  , 1557944.  , 1666591.  , 1577398.  ,
+       1931505.  , 1789961.  , 1863643.  , 1723433.  , 1899849.  ,
+       1977345.  , 2260794.  , 2320110.  , 2456251.  , 2753044.  ,
+       2971286.  , 3125236.  , 3272451.  , 3384670.  , 3295247.  ,
+       3676723.  , 3670435.  , 4247922.  , 4881089.  , 4807430.  ,
+       2994191.84, 2603396.24, 2696877.46])
 
 fig, ax = plt.subplots()
 ax.set_xlim(years.min(), years.max())
 ax.set_ylim(produksi.min(), produksi.max())
 ax.set_xlabel('Year')
 ax.set_ylabel('Produksi')
-ax.set_title('Produksi Beras (Ton) di Provinsi Lampung')
+ax.set_title('Produksi Beras (Ton) di Provinsi Sumatera Selatan')
 plt.grid(True, alpha=0.3)
 
 line, = ax.plot([], [], lw=2)
@@ -53,5 +55,7 @@ def update(frame):
 
 # Create the animation
 ani = FuncAnimation(fig, update, frames=len(years), init_func=init, blit=True, interval=500)
+
+plt.show()
 
 ani.save('produksi_animation.gif', writer='ffmpeg', fps=2)
